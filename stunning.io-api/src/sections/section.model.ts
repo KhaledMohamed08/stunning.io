633db1b3ab8b534f304ce9e1d8e7f7c01bbef844
KhaledMohamed08 <khaledmohamed0896@gmail.com>
2025-07-20 03:06:07 +0300

    refactor createdAt in sored records

--- stunning.io-api/src/sections/section.model.ts
+++ stunning.io-api/src/sections/section.model.ts
@@ -3,9 +3,18 @@ import * as mongoose from 'mongoose';
 export const SectionSchema = new mongoose.Schema({
   prompt: { type: String, required: true },
   sections: { type: [String], required: true },
+  createdAt: { 
+    type: Date, 
+    default: () => {
+      const cairoTime = new Date();
+      cairoTime.setHours(cairoTime.getHours() + 2);
+      return cairoTime;
+    }
+  },
 });
 
 export interface Section {
   prompt: string;
   sections: string[];
+  createdAt: Date;
 }
